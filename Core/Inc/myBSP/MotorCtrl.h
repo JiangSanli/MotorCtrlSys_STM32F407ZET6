82bc3c8fed82c358e4a3cefd1ea9e052d94e7565
JiangSanli <jiangsanli@outlook.com>
2023-08-22 13:32:52 +0800

    revised

--- Core/Inc/myBSP/MotorCtrl.h
+++ Core/Inc/myBSP/MotorCtrl.h
@@ -11,7 +11,7 @@
 #define Encoder2_0position_number 1000		//编码器2:设定0位编码器的计数值，便于对负数进行计算
 #define Encoder3_0position_number 64535		//编码器2:设定0位编码器的计数值，便于对负数进行计算
 
-#define CiFenLi  		// 磁分离电机控制
+//#define CiFenLi  		// 磁分离电机控制
 //#define JiaYangZhen 	// 加样针模块控制，两个42步进电机，不适用编码器控制采用编码器闭环控制
 //#define RZ_stepmotor	// RZ步进电机
 //#define JiaYangZhen_EncoderMode  // 加样针模块编码器控制
@@ -19,7 +19,7 @@
 //#define DushuModule		// PMT读数模块
 //#define L298N_StepMotorCtrl
 //#define DuoTongDao
-//#define QuanxiePVctrl
+#define QuanxiePVctrl
 
 struct MotorDefine {
 	//电机本身机械参数-固定值
